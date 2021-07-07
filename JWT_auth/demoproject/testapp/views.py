4fb4e03051d77b7f99fd36c584e509fc8751ac5b
Shital@06 <cshital237@gmail.com>
2021-07-07 13:19:00 +0530

    jwt created

---
+++ JWT_auth/demoproject/testapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Employee
+from rest_framework.viewsets import ModelViewSet
+from .serializers import EmployeeSerializer
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+
+# Create your views here.
+
+class EmployeeCRUD(ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    authentication_classes = [JSONWebTokenAuthentication,]
+    permission_classes = [IsAuthenticated,]
